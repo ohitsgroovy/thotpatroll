d5c8b8323e06a09333db660e58c8290928dd7bbb
ohitsgroovy <35477887+ohitsgroovy@users.noreply.github.com>
2018-01-16 17:31:50 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.NDAyNjY5MTQwODEwMzk5NzQ0.DT8Jaw.ufznTmJhouE0uG090bTSenL8-kM);
+client.login(process.env.BOT_TOKEN);
